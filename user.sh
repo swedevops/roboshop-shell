12eeda2426a55debd1ea40b58e06ca87476a24ce
swedev99 <swedevops99@gmail.com>
2023-04-19 18:17:12 +0530

    This is a roboshop shell script

--- user.sh
+++ user.sh
@@ -23,7 +23,7 @@ echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
-cp $script_path/user.service /etc/systemd/system/user.service
+cp /root/roboshop-shell/user.service /etc/systemd/system/user.service
 
 echo -e "\e[36m>>>>>>>>> Start User Service <<<<<<<<\e[0m"
 systemctl daemon-reload
