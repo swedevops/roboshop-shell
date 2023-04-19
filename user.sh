99953e742f62f733183cb71be6ed35f3befe6dce
swedev99 <swedevops99@gmail.com>
2023-04-19 13:47:36 +0530

    This is a roboshop shell script

--- user.sh
+++ user.sh
@@ -1,5 +1,6 @@
-script_path=$(realpath "$0")
-# source ${script_path}/common.sh
+script=$(realpath "$0")
+script_path= $(dirname $script)
+ # source $(dirname $script_path)/common.sh
 echo $script_path
 exit
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
