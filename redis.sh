8b10848a326669772524477ea08bde25c4baafd0
swedev99 <swedevops99@gmail.com>
2023-04-21 15:29:27 +0530

    realpath dirname

--- redis.sh
+++ redis.sh
@@ -1,10 +1,7 @@
 script=$(realpath "$0")
 #echo script
 script_path=$(dirname "$script")
-
-
-source $script_path/common.sh
-
+source ${script_path}/common.sh
 echo -e "\e[36m>>>>>>>>> Install Redis Repos <<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
