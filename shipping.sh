6d676b3b68da17bb87de955c335422f2d8fe231d
swedev99 <swedevops99@gmail.com>
2023-04-21 16:47:40 +0530

    realpath dirname

--- shipping.sh
+++ shipping.sh
@@ -27,7 +27,7 @@ echo -e "\e[36m>>>>>>>>> Install MySQL <<<<<<<<\e[0m"
 yum install mysql -y
 
 echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
-mysql -h mysql.swedev99.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql.swedev99.online -uroot -pRoboShop@1 </app/schema/shipping.sql
 
 echo -e "\e[36m>>>>>>>>> Setup SystemD Service <<<<<<<<\e[0m"
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
