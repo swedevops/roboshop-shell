851b2f98ade3753289a665311977e59be5af6bb1
swedev99 <swedevops99@gmail.com>
2023-05-01 20:15:27 +0530

    realpath dirname

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,12 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_user=$1
+if [ -z "$mysql_root_user" ]
+then
+  echo mysql root password missing
+  exit
+  fi
+
 
 echo -e "\e[36m>>>>>>>>> Disable MySQL 8 Version <<<<<<<<\e[0m"
 dnf module disable mysql -y
