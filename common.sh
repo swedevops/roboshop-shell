607fe06c83656d73e7bc5fee0b6a872f1b8c1e6f
swedev99 <swedevops99@gmail.com>
2023-05-01 15:29:37 +0530

    realpath dirname

--- common.sh
+++ common.sh
@@ -2,7 +2,6 @@ app_user=roboshop
 print_head(){
  echo -e "\e[36m>>>>>>>>> $1 <<<<<<<<\e[0m"
 }
-
 fun_nodejs(){
   print_head " Configuring NodeJS repos "
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
@@ -18,21 +17,21 @@ fun_nodejs(){
   mkdir /app
 
   print_head " Download App Content "
-  curl -o /tmp/${function}.zip https://roboshop-artifacts.s3.amazonaws.com/${function}.zip
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
   print_head " Unzip App Content "
-  unzip /tmp/${function}.zip
+  unzip /tmp/${component}.zip
 
   print_head " Install NodeJS Dependencies "
   npm install
   print_head " Create Application Directory "
-  cp ${script_path}/${function}.service /etc/systemd/system/${function}.service
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-  print_head " Start ${function} Service "
+  print_head " Start ${component} Service "
   systemctl daemon-reload
-  systemctl enable ${function}
-  systemctl restart ${function}
+  systemctl enable ${component}
+  systemctl restart ${component}
  }
 
 
