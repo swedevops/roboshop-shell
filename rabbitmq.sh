ab532618fee6e3f7d00fb58aa4ce767cd79f3dc5
swedev99 <swedevops99@gmail.com>
2023-05-01 19:30:46 +0530

    realpath dirname

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,9 +3,9 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_passwd=$1
-if[ -z "$rabbitmq_passwd" ]
+if [ -z "$rabbitmq_passwd" ]
 then
-  echo password is missing
+  echo rabbitmq_password missing
   exit
   fi
  echo -e "\e[36m>>>>>>>>> Setup ErLang Repos <<<<<<<<\e[0m"
