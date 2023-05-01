941c8ee465a67d1e09a35285553a388a6d9d3b83
swedev99 <swedevops99@gmail.com>
2023-05-01 20:46:13 +0530

    realpath dirname

--- payment.sh
+++ payment.sh
@@ -2,7 +2,11 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_passwd=$1
-
+if [ -z "$rabbitmq_passwd" ]
+then
+  echo rabbitmq_passwd password missing
+  exit
+  fi
 echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
 
