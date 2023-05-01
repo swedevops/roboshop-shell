e6c339689c01d53719a958369f41a360c348f42d
swedev99 <swedevops99@gmail.com>
2023-05-01 19:28:24 +0530

    realpath dirname

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,7 +3,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_passwd=$1
-if[ -z "$rabbitmq_passwd"]
+if[ -z "$rabbitmq_passwd" ]
 then
   echo password is missing
   exit
