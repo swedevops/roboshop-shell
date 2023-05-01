156ed98afaf3a80ac6490eaa031356a29e24daef
swedev99 <swedevops99@gmail.com>
2023-05-01 15:35:26 +0530

    realpath dirname

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
