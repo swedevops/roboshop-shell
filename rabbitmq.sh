ec0c93c28946b86bcd53e4a5d5e2dc4c76787ab4
swedev99 <swedevops99@gmail.com>
2023-05-01 20:29:57 +0530

    realpath dirname

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -3,6 +3,12 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 rabbitmq_passwd=$1
+
+if [ -z "$rabbitmq_passwd" ]
+then
+  echo pitchi nayala input ledhu raa
+  exit
+  fi
  echo -e "\e[36m>>>>>>>>> Setup ErLang Repos <<<<<<<<\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 
