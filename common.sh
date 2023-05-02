f67f91a5cb4865490ee08beadf7d5c9293a98598
swedev99 <swedevops99@gmail.com>
2023-05-02 18:43:39 +0530

    code dry

--- common.sh
+++ common.sh
@@ -1,17 +1,18 @@
 app_user=roboshop
+log_file=/tmp/roboshop.log
 func_print_head(){
  echo -e "\e[36m>>>>>>>>> $1 <<<<<<<<\e[0m"
 }
 func_nodejs(){
   func_print_head " Configuring NodeJS repos "
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
   func_status_check $?
    func_print_head " Install NodeJS "
-  yum install nodejs -y
+  yum install nodejs -y &>>$log_file
   func_status_check $?
    func_pre-requisites
   func_print_head " Install NodeJS Dependencies "
-  npm install
+  npm install &>>$log_file
   func_status_check $?
 
   func_systemd_setup
@@ -19,31 +20,32 @@ func_nodejs(){
  }
  func_status_check(){
    if [ $1 -eq 0 ]; then
-     echo -e "/e[32msucess\e[0m"
+     echo -e "\e[32m sucess \e[0m"
      else
-       echo -e "/e[31mfailure\e[0m"
+       echo -e "\e[31m failure \e[0m"
+       echo refer the log
        exit 1
        fi
  }
 func_schema_setup(){
   if [ "$schema_setup" == "mongo" ]; then
   echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<\e[0m"
-  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
   func_status_check $?
 
   echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<\e[0m"
-  yum install mongodb-org-shell -y
+  yum install mongodb-org-shell -y &>>$log_file
   func_status_check $?
 
   echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
-  mongo --host mongodb-dev.swedev99.online </app/schema/${component}.js
+  mongo --host mongodb-dev.swedev99.online </app/schema/${component}.js &>>$log_file
   func_status_check $?
  fi
 
  if [ "$schema_setup" == "mysql" ]; then
 
       func_print_head "Install MySQL"
-      yum install mysql -y
+      yum install mysql -y &>>$log_file
       func_status_check $?
 
       func_print_head "Load Schema"
@@ -55,43 +57,43 @@ func_schema_setup(){
 func_pre-requisites()
 {
   func_print_head "Create App User"
-     useradd ${app_user} &>/tmp/roboshop.log
+     useradd ${app_user} &>/tmp/roboshop.log &>>$log_file
      func_status_check $?
      
      func_print_head "Create App Directory"
      rm -rf /app
-     mkdir /app
+     mkdir /app &>>$log_file
      func_status_check $?
      func_print_head " Download App Content"
-     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
       func_status_check $?
       func_print_head "Extract App Content"
      cd /app
-     unzip /tmp/${component}.zip
+     unzip /tmp/${component}.zip &>>$log_file
      func_status_check $?
 
 }
 
  func_systemd_setup(){
     func_print_head " Setup SystemD Service"
-      cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+      cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
       func_status_check $?
       func_print_head " Start ${component} Service"
       systemctl daemon-reload
       systemctl enable ${component}
-      systemctl restart ${component}
+      systemctl restart ${component} &>>$log_file
       func_status_check $?
  }
  func_java(){
 
    func_print_head "Install Maven"
-   yum install maven -y
+   yum install maven -y &>>$log_file
    func_status_check $?
    func_pre-requisites
    func_print_head "Download Maven Dependencies"
-         mvn clean package
+         mvn clean package &>>$log_file
          func_status_check $?
-         mv target/${component}-1.0.jar ${component}.jar
+         mv target/${component}-1.0.jar ${component}.jar &>>$log_file
          func_status_check $?
          func_systemd_setup
          func_schema_setup
@@ -101,16 +103,16 @@ func_pre-requisites()
 func_python()
 {
   echo -e "\e[36m>>>>>>>>> Install Python <<<<<<<<\e[0m"
-  yum install python36 gcc python3-devel -y
+  yum install python36 gcc python3-devel -y &>>$log_file
   func_status_check $?
   func_pre-requisites
 
   echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<\e[0m"
-  pip3.6 install -r requirements.txt
+  pip3.6 install -r requirements.txt &>>$log_file
   func_status_check $?
 
   echo -e "\e[36m>>>>>>>>>Setup SystemD Service <<<<<<<<\e[0m"
-  sed -i -r "s|rabbitmq_passwd|${rabbitmq_passwd}|" ${script_path}/payment.service
+  sed -i -r "s|rabbitmq_passwd|${rabbitmq_passwd}|" ${script_path}/payment.service &>>$log_file
   func_status_check $?
   func_systemd_setup
 }
