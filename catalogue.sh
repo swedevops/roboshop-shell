6f9ebd0712f29e8dad03be67f379187eb2cc6ebf
swedev99 <swedevops99@gmail.com>
2023-05-02 17:05:54 +0530

    code dry

--- catalogue.sh
+++ catalogue.sh
@@ -2,8 +2,9 @@ script=$(realpath "$0")
 #echo script
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+
 component=catalogue
-func_schema_setup=mongo
+schema_setup=mongo
 func_nodejs
 
 
