27d390dd418d1a66f8534482cef557f77e0bb14e
swedev99 <swedevops99@gmail.com>
2023-05-18 18:16:21 +0530

    code dry

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -11,23 +11,23 @@ then
   fi
 func_print_head " Download App Content - setup erland repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
-func_stat_check $?
+func_status_check $?
 func_print_head " Install erlang "
 yum install erlang -y &>>$log_file
-func_stat_check $?
+func_status_check $?
 func_print_head " Download App Content - setup rabbitmq repos "
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
-func_stat_check $?
+func_status_check $?
 func_print_head "  Install rabbitmq  "
 yum install rabbitmq-server -y &>>$log_file
-func_stat_check $?
+func_status_check $?
 func_print_head " Start rabbitmq Service "
 systemctl enable rabbitmq-server &>>$log_file
 systemctl start rabbitmq-server &>>$log_file
-func_stat_check $?
+func_status_check $?
 func_print_head "Add application user  "
 rabbitmqctl add_user roboshop ${rabbitmq_appuser_password} &>>$log_file
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
-func_stat_check $?
+func_status_check $?
 
 
